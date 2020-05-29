2c44aa1bb74ef3c9d7e1b18713c5435a9f603668
fruxc <hammadansari17@gmail.com>
2020-05-29 19:58:42 +0530

    Started working again

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
 
 /* Mongoose Settings */
 
@@ -35,16 +40,6 @@ connect.then(
 
 var app = express();
 
-const dishRouter = require("./routes/dishRouter");
-const promoRouter = require("./routes/promoRouter");
-const leaderRouter = require("./routes/leaderRouter");
-
-/* REST API */
-
-app.use("/leaders", leaderRouter);
-app.use("/dishes", dishRouter);
-app.use("/promotions", promoRouter);
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -52,11 +47,48 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(cookieParser("12345-67890-09876-54321"));
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
+
+/* Authentication */
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+function auth(req, res, next) {
+  console.log(req.session);
+  if (!req.session.user) {
+    var err = new Error("Missing authorization header");
+    err.status = 401;
+    return next(err);
+  } else {
+    if (req.session.user === "authenticated") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+      err.status = 401;
+      return next(err);
+    }
+  }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+/* REST API */
+
+app.use("/leaders", leaderRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
